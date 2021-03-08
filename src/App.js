1dcd4ae59a14a048c72166e840d60d65fd9023d7
Zioq <jr.roberthan@gmail.com>
2021-03-08 13:46:00 -0800

    update callback function for setState's anschronous function

--- src/App.js
+++ src/App.js
@@ -4,8 +4,11 @@ import { CardList } from "./components/CardList/CardList";
 class App extends Component {
   constructor() {
     super();
+
+    // State: which is that changes because of user interaction
     this.state = {
       monsters: [],
+      searchField: "",
     };
   }
 
@@ -15,11 +18,20 @@ class App extends Component {
       .then((users) => this.setState({ monsters: users }));
   }
 
+  // State gets turns into props into all these componenets that we pass it down.
   render() {
     return (
       <div className="App">
+        <input
+          type="search"
+          placeholder="search monsters"
+          onChange={(e) =>
+            this.setState({ searchField: e.target.value }, () =>
+              console.log(this.state)
+            )
+          }
+        />
         <CardList monsters={this.state.monsters} />
-
       </div>
     );
   }
